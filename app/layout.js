9b91587a583448521479035efa877f4c69140ce9
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-12-09 22:27:41 +0000

    Update RootLayout with, background, and layout adjustments

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from '@/app/_components/Navigation';
 import '@/app/_styles/globals.css';
 
 import { Josefin_Sans } from 'next/font/google';
+import Header from './_components/Header';
 const josefin = Josefin_Sans({
   subsets: ['latin'],
   display: 'swap',
@@ -20,14 +21,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} text-primary-100 min-h-screen bg-primary-950`}
+        className={`${josefin.className} text-primary-100 min-h-screen bg-primary-950 flex flex-col antialiased`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by Hotel</footer>
+        <Header />
+        <div className=" flex-1 px-8 py-12 ">
+          <main className=" max-w-7xl  mx-auto ">{children}</main>
+        </div>
       </body>
     </html>
   );
