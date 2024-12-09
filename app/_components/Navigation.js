7fa82c82dad0acd3c0f80a91fc59b98b1173d78d
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-12-09 22:07:23 +0000

    Create Navigation component

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,30 +1,3 @@
-// export default function Navigation() {
-//   return (
-//     <nav className="z-10 text-xl">
-//       <ul className="flex gap-16 items-center">
-//         <li>
-//           <a href="/cabins" className="hover:text-accent-400 transition-colors">
-//             Cabins
-//           </a>
-//         </li>
-//         <li>
-//           <a href="/about" className="hover:text-accent-400 transition-colors">
-//             About
-//           </a>
-//         </li>
-//         <li>
-//           <a
-//             href="/account"
-//             className="hover:text-accent-400 transition-colors"
-//           >
-//             Your account
-//           </a>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// }
-
 import Link from 'next/link';
 
 export default function Navigation() {
